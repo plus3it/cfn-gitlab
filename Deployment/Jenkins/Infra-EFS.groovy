7fa9d730aa170122f5963a560c83847bfdf7a2e1
Thomas H Jones II <ferricoxide@users.noreply.github.com>
2019-01-17 11:09:15 -0500

    Jenkins jobs (#23)
    * Copy updated provisioning-logic in standalone- and autoscale-templates
    * Add GitHub issue-templates to project
    * Add a 'no integrated-storage' infra-template
    * Remove redundant backup job setup
    * Move persisted-share setup from _config to _instprep
    * Add ACM/IAM selector for certificate-hosting service
    * Make execution of watchmaker toggleable
    * Add a Jenkins job for the storage-less infrastructure parent template
    * Add Jenkins job for deploying EFS-enabled nested infra-stack
    * Add Jenkins job for GitLab-hosting standalone EC2 instance
    * Add Job-definition to drive autoscaling template
    * Provide explanation for absence rather than create a Jenkins job-definition

--- Deployment/Jenkins/Infra-EFS.groovy
+++ Deployment/Jenkins/Infra-EFS.groovy
@@ -28,36 +28,35 @@ pipeline {
         string(name: 'GitProjUrl', description: 'SSH URL from which to download the Jenkins git project')
         string(name: 'GitProjBranch', description: 'Project-branch to use from the Jenkins git project')
         string(name: 'CfnStackRoot', description: 'Unique token to prepend to all stack-element names')
-        string(name: 'SecurityGroupTemplate', description: 'URL to the child-template for creating the GitLab SecurityGroups')
-        string(name: 'GitlabBucketTemplate', description: 'URL to the child-template used to create the GitLab S3-bucket')
-        string(name: 'GitlabIamTemplate', description: 'URL to the child-template for creating the IAM roles used to access the GitLab S3 bucket(s)')
-        string(name: 'GitlabEfsTemplate', description: 'URL to the child-template for creating the GitLab EFS service')
-        string(name: 'GitlabRdsTemplate', description: 'URL to the child-template for creating the GitLab RDS service')
-        string(name: 'GitlabElbTemplate', description: 'URL to the child-template for creating the public-facing GitLab ELB')
-        string(name: 'TargetVPC', description: 'ID of the VPC to deploy GitLab components into')
-        string(name: 'PgsqlVersion', description: 'Version of the PostGreSQL database to deploy')
-        string(name: 'DbInstanceType', defaultValue: 'db.m4.large', description: 'Amazon RDS instance type')
-        string(name: 'DbDataSize', description: 'Size in GiB of the RDS table-space to create')
-        string(name: 'DbNodeName', description: 'NodeName to assign to the GitLab RDS instance')
-        string(name: 'DbInstanceName', description: 'Instance-name of the GitLab database')
-        string(name: 'DbAdminName', description: 'Name of the GitLab master database-user')
-        password(name: 'DbAdminPass', description: 'Password of the GitLab master database-user')
-        string(name: 'CloudwatchBucketName', description: 'S3 bucket from which to fetch CloudWatch log-agent')
-        string(name: 'GitlabBucket', description: 'S3 Bucket used by GitLab EC2 host to store files for long-term retention')
-        string(name: 'Folder', defaultValue: 'Backups', description: 'Folder in S3 Bucket to host backups of GitLab config-data')
-        string(name: 'TierToGlacierDays', defaultValue: '7', description: 'Number of days to retain objects in standard storage tier')
-        string(name: 'FinalExpirationDays', defaultValue: '30', description: 'Number of days to retain objects before aging them out of the bucket')
-        string(name: 'RetainIncompleteDays', defaultValue: '2', description: 'Number of days to retain objects that were not completely uploaded')
-        string(name: 'BucketInventoryTracking', defaultValue: 'false',description: '(Optional) Whether to enable generic bucket inventory-tracking. Requires setting of the "ReportingBucket" parameter')
-        string(name: 'ReportingBucket', description: '(Optional) Destination for storing analytics data')
-        string(name: 'ConfigBucketPath', description: 'S3 bucket-path to GitLab configuration template-file')
-        string(name: 'RolePrefix', description: '(Optional) Prefix to apply to IAM role to make things a bit prettier')
-        string(name: 'HaSvcSubnets', description: 'Private subnets to create RDS and EFS listeners on')
-        string(name: 'GitlabElbSubnets', description: 'User-facing subnets to create ELB listeners on')
-        string(name: 'GitLabListenerCert', description: 'Amazon Certificate Manager object-ID for ELB SSL certificate')
-        choice(name: 'GitLabPassesSsh', choices:'false\ntrue', description: 'Whether public-facing ELB should forward SSH traffic to GitLab EC2 instance')
-        string(name: 'GitLabServicePort', defaultValue: '80', description: 'TCP Port number that the GitLab service listens on')
-        string(name: 'GitRepoShareType', description: 'Type of network share hosting persisted git repository content')
+        choice(name: 'BucketInventoryTracking', choices: 'true\nfalse', description: '(Optional: ignored if BackupReportingBucket is unset) Whether to enable generic bucket inventory-tracking')
+        choice(name: 'CertHostingService', choices:'ACM\nIAM', description: 'Select AWS service that is hosting the SSL certificate.')
+        string(name: 'CloudwatchBucketName', description: 'Name of the S3 Bucket hosting the CloudWatch agent archive files', defaultValue: 'amazoncloudwatch-agent')
+        string(name: 'ConfigBucketPath', description: 'S3 bucket-path to GitLab configuration template-file.')
+        string(name: 'DbAdminName', description: 'Name of the GitLab master database-user.')
+        string(name: 'DbAdminPass', description: 'Password of the GitLab master database-user.')
+        string(name: 'DbDataSize', description: 'Size in GiB of the RDS table-space to create.')
+        string(name: 'DbInstanceName', description: 'Instance-name of the GitLab database.')
+        string(name: 'DbInstanceType', description: 'Amazon RDS instance type')
+        string(name: 'DbNodeName', description: 'NodeName of the GitLab database.')
+        string(name: 'FinalExpirationDays', description: 'Number of days to retain objects before aging them out of the bucket', defaultValue: '30')
+        string(name: 'GitLabListenerCert', description: 'Name/ID of the ACM-managed SSL Certificate to protect public listener.')
+        choice(name: 'GitLabPassesSsh', choices: 'true\nfalse', description: 'Whether to allow SSH passthrough to GitLab master.')
+        string(name: 'GitLabServicePort', description: 'TCP Port number that the GitLab host listens to.', defaultValue: '80')
+        string(name: 'GitlabBucket', description: 'S3 Bucket to host GitLab files')
+        string(name: 'GitlabBucketTemplate', description: 'URL to the child-template for creating the gitlab-bucket.')
+        string(name: 'GitlabEfsTemplate', description: 'URL to the child-template for creating the GitLab EFS service.')
+        string(name: 'GitlabElbSubnets', description: 'Select three subnets - each from different Availability Zones.')
+        string(name: 'GitlabElbTemplate', description: 'URL to the child-template for creating the GitLab service-ELB')
+        string(name: 'GitlabIamTemplate', description: 'URL to the child-template for creating the GitLab S3 bucket(s) IAM roles.')
+        string(name: 'GitlabRdsTemplate', description: 'URL to the child-template for creating the GitLab RDS service.')
+        string(name: 'HaSvcSubnets', description: 'Select three subnets - each from different Availability Zones.')
+        string(name: 'PgsqlVersion', description: 'The X.Y.Z version of the PostGreSQL database to deploy.', defaultValue: '9.6.10')
+        string(name: 'ReportingBucket', description: '(Optional) Destination for storing analytics data. Must be provided in ARN format.')
+        string(name: 'RetainIncompleteDays', description: 'Number of days to retain objects that were not completely uploaded.', defaultValue: '2')
+        string(name: 'RolePrefix', description: 'Prefix to apply to IAM role to make things a bit prettier (optional).', defaultValue: 'INSTANCE')
+        string(name: 'SecurityGroupTemplate', description: 'URL to the child-template for creating the GitLab SecurityGroups.')
+        string(name: 'TargetVPC', description: 'ID of the VPC to deploy GitLab components into.')
+        string(name: 'TierToGlacierDays', description: 'Number of days to retain objects in standard storage tier.', defaultValue: '6')
     }
 
     stages {
@@ -67,12 +66,16 @@ pipeline {
                 git branch: "${GitProjBranch}",
                     credentialsId: "${GitCred}",
                     url: "${GitProjUrl}"
-                writeFile file: 'service-infra.parms.json',
+                writeFile file: 'gitlab-infra-efs.parms.json',
                     text: /
                         [
                             {
                                 "ParameterKey": "BucketInventoryTracking",
                                 "ParameterValue": "${env.BucketInventoryTracking}"
+                            },
+                            {
+                                "ParameterKey": "CertHostingService",
+                                "ParameterValue": "${env.CertHostingService}"
                             },
                             {
                                 "ParameterKey": "CloudwatchBucketName",
@@ -111,10 +114,6 @@ pipeline {
                                 "ParameterValue": "${env.FinalExpirationDays}"
                             },
                             {
-                                "ParameterKey": "Folder",
-                                "ParameterValue": "${env.Folder}"
-                            },
-                            {
                                 "ParameterKey": "GitLabListenerCert",
                                 "ParameterValue": "${env.GitLabListenerCert}"
                             },
@@ -125,10 +124,6 @@ pipeline {
                             {
                                 "ParameterKey": "GitLabServicePort",
                                 "ParameterValue": "${env.GitLabServicePort}"
-                            },
-                            {
-                                "ParameterKey": "GitRepoShareType",
-                                "ParameterValue": "${env.GitRepoShareType}"
                             },
                             {
                                 "ParameterKey": "GitlabBucket",
@@ -194,7 +189,7 @@ pipeline {
                     /
                 sh '''#!/bin/bash
                     printf "Validating param-file syntax... "
-                    SYNTAXCHK=$( python -m json.tool service-infra.parms.json > /dev/null 2>&1 )$?
+                    SYNTAXCHK=$( python -m json.tool gitlab-infra-efs.parms.json > /dev/null 2>&1 )$?
                     if [[ ${SYNTAXCHK} -eq 0 ]]
                     then
                        echo "syntax is valid"
@@ -246,8 +241,8 @@ pipeline {
                         echo "Attempting to create stack ${CfnStackRoot}..."
                         aws --region "${AwsRegion}" cloudformation create-stack --stack-name "${CfnStackRoot}" \
                           --disable-rollback --capabilities CAPABILITY_NAMED_IAM \
-                          --template-body file://Templates//make_gitlab_parent-infra-EFS.tmplt.json \
-                          --parameters file://service-infra.parms.json
+                          --template-body file://Templates/make_gitlab_parent-infra-EFS.tmplt.json \
+                          --parameters file://gitlab-infra-efs.parms.json
 
                         sleep 15
 
---
+++ Deployment/Jenkins/Infra-gluster.md
@@ -0,0 +1,3 @@
+### Deploying Gluster with Jenkins
+
+Note: while the GitLab deployments expect to use either EFS or Gluster to support persisting of repository-data external to deployment of GitLab nodes, automation of Gluster clusters is not part of _this_ project. Therefore, this project includes no Jenkins job-definitions.
